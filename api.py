76857b798ac03b166a80897f125b32ea728c041a
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-05-01 06:49:51 -0400

    Wrote function to make rue-api requests. Tested with different calls.

--- api.py
+++ api.py
@@ -1,17 +1,28 @@
 import requests
 import json
 
-## Just testing out how we'll make the API requests
+## Make a request to the Rue server API 
+def rue_api_request(uri, url= None, category=None, verbose=False):
+    payload = {'URL': url, 'Category': category}
+    r = requests.post(uri, json=payload)
 
-r = requests.get('http://localhost:8080/get-feedstore')
-print(r.status_code)
-print(r.json())
+    if verbose:
+        print("\nuri:", uri, "\n-----------------------------")
+        print(r.status_code)
+        print(r.json())
 
-# More complicated request... has a payload
-payload = {'URL': "http://ryan.himmelwright.net/post/index.xml"}
-r = requests.post('http://localhost:8080/get-all-feeditem-data', json=payload)
-print(r.status_code)
-print(r.json())
+    return(r)
 
 
+## Test Calls
+rue_api_request("http://localhost:8080/get-feedstore"
+                , verbose= True)
 
+rue_api_request("http://localhost:8080/add-feed"
+                , url= "http://ryan.himmelwright.net/post/index.xml"
+                , category= "Development"
+                , verbose= True)
+
+rue_api_request("http://localhost:8080/get-all-feeditem-data"
+                , url= "http://ryan.himmelwright.net/post/index.xml"
+                , verbose= True)
