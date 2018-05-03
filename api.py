688738566b4eaf373c5b84891d084a8cbc59372c
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-05-02 21:22:19 -0400

    Pushing some changed I made on the plane

--- api.py
+++ api.py
@@ -18,11 +18,11 @@ def rue_api_request(uri, url= None, category=None, verbose=False):
 rue_api_request("http://localhost:8080/get-feedstore"
                 , verbose= True)
 
-rue_api_request("http://localhost:8080/add-feed"
-                , url= "http://ryan.himmelwright.net/post/index.xml"
-                , category= "Development"
-                , verbose= True)
+# rue_api_request("http://localhost:8080/add-feed"
+#                 , url= "http://ryan.himmelwright.net/post/index.xml"
+#                 , category= "Development"
+#                 , verbose= True)
 
-rue_api_request("http://localhost:8080/get-all-feeditem-data"
-                , url= "http://ryan.himmelwright.net/post/index.xml"
-                , verbose= True)
+# rue_api_request("http://localhost:8080/get-all-feeditem-data"
+#                 , url= "http://ryan.himmelwright.net/post/index.xml"
+#                 , verbose= True)
--- main.py
+++ main.py
@@ -18,11 +18,12 @@ class Menu(object):
         self.position = 0
         self.items = items
         self.items.append(('exit','exit'))
+        print(items)
 
     def navigate(self, n):
         self.position += n
         if self.position < 0:
-            self.position = 0
+            self.position = 0 
         elif self.position >= len(self.items):
             self.position = len(self.items)-1
 
@@ -40,16 +41,18 @@ class Menu(object):
                 else:
                     mode = curses.A_NORMAL
 
-                msg = '%d. %s' % (index, item[0])
+                msg = '%d. %s (%s)' % (index, item[0], str(item[1]))
                 self.window.addstr(1+index, 1, msg, mode)
 
             key = self.window.getch()
 
             if key in [curses.KEY_ENTER, ord('\n')]:
+                ## If last item in list, break
                 if self.position == len(self.items)-1:
                     break
                 else:
                     self.items[self.position][1]()
+
 
             elif key == curses.KEY_UP:
                 self.navigate(-1)
@@ -70,7 +73,7 @@ class MyApp(object):
 
         ## Panel Menu for adding a new Feed
         add_feed_items = [
-                ('Feed URL', curses.beep),      ## These need to be input fields
+                ('Feed URL', curses.flash),      ## These need to be input fields
                 ('Feed Category', curses.flash), ## These need to be input fields
                 ('Add Feed', curses.flash)
                 ]
