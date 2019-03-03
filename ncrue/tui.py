b04151453d4f7712f5152fd18fb66e38e72e7fed
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-03-03 08:33:55 -0500

    Setup curses safe wrapper

--- ncrue/tui.py
+++ ncrue/tui.py
@@ -3,6 +3,8 @@
 """Curse Rue Text User Interface"""
 
 from argparse import ArgumentParser
+import curses
+import time
 from . import __version__
 
 
@@ -18,11 +20,20 @@ class TUI():
                                         Rue RSS feeds in ncurses')
         self.parser.add_argument('--version', action='version',
                                  version=__version__)
+        self.stdscr = None
 
-    def draw_window(self):
-        """Draw new window?"""
+    def run_curses_loop(self, window):
+        """Run the curses sequence loop"""
+        self.stdscr = window
+        self.stdscr.refresh()
+        self.stdscr.addstr(20, 10, 'Hello World!')
+        self.stdscr.refresh()
+        time.sleep(2)
 
-    def main(self):
+    def main(self, in_args):
         """Launch main window"""
+        print(in_args)
         args = self.parser.parse_args()
         print(f"tui main: {args}")
+        curses.wrapper(self.run_curses_loop)
+        print('Exited Curses.')
